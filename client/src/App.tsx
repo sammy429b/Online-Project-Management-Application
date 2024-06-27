f5935632576cb789f32e164dcc481f07cce13443
sammy429b <samarthnbahirgonde49@gmail.com>
2024-06-27 18:34:51 +0530

    protected routes created for user authorization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,19 +4,22 @@ import Main from "./pages/Main";
 import Dashboard from "./components/Dashboard";
 import ProjectList from "./components/ProjectList";
 import CreateProject from "./components/CreateProject";
+import PrivateRoute from "./utils/ProtectesRoutes";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/" element={<Main/>}> 
-          <Route index element={<Dashboard/>} />
-          <Route path="/project-list" element={<ProjectList/>} />
-          <Route path="/create-project" element={<CreateProject/>} />
-         </Route>
-      </Routes>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Main />}>
+              <Route index element={<Dashboard />} />
+              <Route path="/project-list" element={<ProjectList />} />
+              <Route path="/create-project" element={<CreateProject />} />
+            </Route>
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
 
