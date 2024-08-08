b0bdccb085ec28f5aeb86afeb58b48e01a9272cf
sammy429b <samarthnbahirgonde49@gmail.com>
2024-08-08 17:13:36 +0530

    http proxy added

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import authRoute from './routes/auth.route';
 import projectRoute from './routes/project.route';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app: Express = express();
 dotenv.config();
@@ -12,8 +13,6 @@ dotenv.config();
 const port = process.env.SERVER_PORT || 5000;
 const accessOrigin = process.env.ORIGIN || 'http://localhost:5173';
 
-app.use(express.json());
-app.use(cookieParser());
 
 const corsOptions = {
     origin: accessOrigin,
@@ -22,9 +21,25 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization']
 };
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
 // Routes
 app.use("/", projectRoute);
 app.use("/", authRoute);
+
+app.use(
+    '/weather',
+    createProxyMiddleware({
+        target: accessOrigin,
+        changeOrigin: true,
+        pathRewrite: {
+            '^/weather': '', // This rewrites /weather to /
+        },
+    })
+);
+
+
 
 app.get("/", (req: Request, res: Response) => {
     res.send("TypeScript with Express");
